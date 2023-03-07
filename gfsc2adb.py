ef6699ae51c0e5c2dadf1968b8acfcf2c3136f6f
Michael Chen <mjchen@Michaels-MacBook-Pro.local>
2023-03-07 14:27:47 -0800

    Swapped cx_Oracle out for oracledb, works

--- gfsc2adb.py
+++ gfsc2adb.py
@@ -4,7 +4,8 @@ import urllib.request
 import os
 import csv
 import time
-import cx_Oracle
+import oracledb
+import traceback
 
 ##### random helper methods #####
 
@@ -37,48 +38,64 @@ def write_to_file(table): # currently unused
 
 DB_USER = os.environ("DB_USER") # change this to grab info from OCI vault
 DB_PASS = os.environ("DB_PASS")
-DB = "nasadw_high"
+DB_SERVICE = "(description= (retry_count=15)(retry_delay=3)\
+(address=(protocol=tcps)(port=1522)(host=adb.eu-frankfurt-1.oraclecloud.com))\
+(connect_data=(service_name=g6aa31b0b04d2f7_nasadw_high.adb.oraclecloud.com))\
+(security=(ssl_server_dn_match=yes)))" # you can find this string in the tnsnames.ora file. service used is "nasadw_high"
 DB_SCHEMA = "STARLINK_USER"
+WALLET_LOC = os.environ("WALLET_LOC") # location of the ewallet.pem file from the wallet
+WALLET_PASS = os.environ("WALLET_PASS") # password for the wallet
 
-class DatabasePool:
-    def __init__(self):
-        """Constructor method to create the database session pool
-        """
-        self.pool = cx_Oracle.SessionPool(
-            user=DB_USER,
-            password=DB_PASS,
-            dsn=DB,
-            min=15,
-            max=15,
-            increment=0,
-            encoding="UTF-8",
-            threaded=True,
-            getmode=cx_Oracle.SPOOL_ATTRVAL_WAIT)
-
-    def get(self):
-        """Getter method that returns the pool
-
-        Returns:
-            cx_Oracle.SessionPool: database pool
-        """
-        return self.pool
+##### SQL definitions #####
+SQL_getLastTime = f"""
+SELECT *
+FROM (
+    SELECT *
+    FROM {DB_SCHEMA}.GFSC_DATA
+    ORDER BY LAST_MODIFIED DESC
+)
+WHERE ROWNUM = 1
+"""
 
 ##### main method #####
 
 def main():
     base_url = "https://is.sci.gsfc.nasa.gov/gsfcdata/jpss1/viirs/level2/"
     qparams = "?C=S;O=A"
-    htmldata = get_data(base_url + qparams)
+    print(f"{get_time()}|Retrieving data...")
+    #htmldata = get_data(base_url + qparams)
+    with open("/Users/mjchen/Desktop/Innovation/Accenture_Satellite_Project/page.html") as f:
+        htmldata = f.read()
     soup = BeautifulSoup(htmldata, 'html.parser')
 
+    print(f"{get_time()}|Transforming data into table...")
     table = grab_table(soup)
 
-    pool = DatabasePool.get()
-    connection = pool.acquire()
-    connection.current_schema = DB_SCHEMA
-    cursor = connection.cursor()
-    pass
+    print(f"{get_time()}|Initializing database connection...")
+    connection = oracledb.connect(
+        user = DB_USER,
+        password = DB_PASS,
+        dsn = DB_SERVICE,
+        wallet_location = WALLET_LOC,
+        wallet_password = WALLET_PASS)
+    
+    # get the most recently inserted item
+    print(f"{get_time()}|Retrieving last insert time...")
+    last_insert_time = None
+    try:
+        with connection.cursor() as cursor:
+            resp = []
+            for item in cursor.execute(SQL_getLastTime):
+                resp += [item]
+            print(f"Response: {resp}")
+            last_insert_time = resp[0]
+    except:
+        print(f"{get_time()}|Something went wrong. Exception summary:\n{traceback.format_exc()}")
+    finally:
+        connection.close()
 
+    # filter data by last insert time
+    print(last_insert_time)
 
 if __name__ == "__main__":
     main()
