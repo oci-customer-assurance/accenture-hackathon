3b22165bae6c09a23a90ed4c2a646cec48d8f912
Michael Chen <mjchen@Michaels-MacBook-Pro.local>
2023-03-14 17:09:39 -0700

    Sample file that can access the cloud given a valid connection string

---
+++ azureblob2adw.py
@@ -0,0 +1,41 @@
+import os
+from azure.storage.blob import BlobServiceClient
+
+conn_str = #os.getenv("az_storage_conn_str")
+container_name = "michael-chen-container"
+blob_name = "burrito.png"
+file_loc = "/Users/mjchen/Desktop/misc/burrito_p2.png"
+
+# Initialize a BlobServiceClient object
+print("Initializing BlobServiceClient")
+blob_service_client = BlobServiceClient.from_connection_string(conn_str)
+
+# Create a container for the blob
+print("Creating container")
+container_client = blob_service_client.create_container(container_name)
+
+# # Initialize a container from its name. Only works on existing containers
+# print("Initializing container client")
+# container_client = blob_service_client.get_container_client(container_name)
+
+# Initialize a blob client
+print("Initializing blob client")
+blob_client = blob_service_client.get_blob_client(
+	container = container_name,
+	blob = blob_name)
+
+# Write a file to the blob client
+with open(file_loc, "rb") as f:
+	blob_client_upload_blob_response = blob_client.upload_blob(f)
+
+# Print the blob names in the container
+print("Listing blobs:")
+blob_list = container_client.list_blobs()
+for blob in blob_list:
+	print(blob.name)
+
+# Download a blob to local
+with open("/Users/mjchen/Desktop/BURRITO.png", "wb") as f:
+	f.write(container_client.download_blob(blob_name).readall())
+
+print("Done.")
